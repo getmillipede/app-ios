647d35167c50dd88d1811e36d1f1c119691be97d
Edouard Bonlieu <ebonlieu@ocs.online.net>
2015-07-26 01:18:58 +0200

    Contact list bugged WIP!

---
+++ ContactList.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var AddressBook = require('react-native-addressbook'),
+  Contact = require('./Contact'),
+  React = require('react-native'),
+  Router = require('react-native-router');
+
+var {
+  ActivityIndicatorIOS,
+  AppRegistry,
+  Component,
+  ListView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} = React;
+
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 10
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  listView: {
+    backgroundColor: '#F5FCFF'
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+
+class ContactList extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        isLoading: true,
+        dataSource: new ListView.DataSource({
+          rowHasChanged: (row1, row2) => row1 !== row2
+        })
+      };
+  }
+
+  componentDidMount() {
+    this.getContact();
+  }
+
+
+  filterMobilePhone(item) {
+    if (item.phoneNumbers && item.phoneNumbers[0]) {
+      return true;
+    }
+    return false;
+  }
+
+  getContact() {
+    AddressBook.getContacts( (err, contacts) => {
+      var contactsWithMobile = contacts.filter(this.filterMobilePhone)
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(contactsWithMobile),
+        isLoading: false
+      });
+    })
+  }
+
+  goToContact(contact) {
+    this.props.toRoute({
+      name: "Send a millipede",
+      component: Contact,
+      data: contact,
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return this.renderLoadingView();
+    }
+ 
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderContact.bind(this)}
+        style={styles.listView}
+      />
+    )
+  }
+
+  renderContact(contact) {
+    return (
+      <TouchableHighlight onPress={() => this.goToContact(contact)} underlayColor='#dddddd'>
+        <View>
+          <View style={styles.container}>
+            <View style={styles.rightContainer}>
+            <Text style={styles.title}>{contact.firstName} {contact.lastName}</Text>
+            <Text style={styles.phone}>{contact.phoneNumbers[0].number}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicatorIOS
+          size='large'/>
+        <Text>
+          Loading contacts...
+        </Text>
+      </View>
+    );
+  }
+};
+
+module.exports = ContactList
--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-var React = require('react-native');
+var AddressBook = require('react-native-addressbook'),
+  ContactList = require('./ContactList'),
+  React = require('react-native'),
+  Router = require('react-native-router');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } = React;
 
-var millipede = React.createClass({
+var styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#5cafec'
+  },
+  container: {
+    marginTop:50,
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'rgb(253,253,253)',
+  },
+  button: {
+    padding: 20,
+    marginBottom: 20,
+    fontSize: 24,
+    borderRadius: 4,
+    borderWidth: 0.5,
+    color: '#d6d7da',
+    borderColor: '#d6d7da',
+  },
+  text: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 32,
+  },
+});
+
+var homePage = React.createClass({
+  goToContactList: function() {
+    this.props.toRoute({
+      name: "Contacts",
+      component: ContactList
+    });
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+     <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.goToContactList() }>
+          <Text style={styles.button}>Send a millipede</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>
+          ╚⊙ ⊙╝
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.text}>
+          ╚═(███)═╝
+          ╚═(███)═╝
+          ╚═(███)═╝
+          ╚═(███)═╝
+          ╚═(███)═╝
+          ╚═(███)═╝
         </Text>
       </View>
-    );
+    )
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var homeRoute = {
+  name: 'Millipede',
+  component: homePage
+};
+
+var millipede = React.createClass({
+  render() {
+    return (
+      <Router firstRoute={homeRoute} />
+    )
+  }
 });
 
 AppRegistry.registerComponent('millipede', () => millipede);
